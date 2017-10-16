1410e8c1941a49cd2110a0060ff5db58ff25123e
FranSalom <fransalomt@gmail.com>
2017-10-16 10:54:48 -0400

    Agregamos el menu desplegable

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Francisco Salom on 16/10/17.
+//  Copyright © 2017 Francisco Salom. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+    }
+
+}
